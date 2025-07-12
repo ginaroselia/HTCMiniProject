9e58a9e78a63605c7e31a9343f3aa5dbd66c9dd0
Kumanan97 <kumanan9703@gmail.com>
2025-07-12 12:05:27 +0800

    frontend

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+
+const Form = () => {
+	const [file, setFile] = useState(null);
+	const [id, setId] = useState('');
+
+	// Change the url
+	const BASE_URL = 'http://localhost:3000';
+
+	const handleImageChange = (e) => {
+		setFile(e.target.files[0]);
+	};
+
+	// add the url endpoint
+	const handleImageSubmit = async (e) => {
+		e.preventDefault();
+		if (!file) {
+			alert('Please upload an image.');
+			return;
+		}
+
+		const formData = new FormData();
+		formData.append('image', file);
+
+		try {
+			const response = await fetch(`${BASE_URL}`, {
+				method: 'POST',
+				body: formData,
+			});
+			const result = await response.json();
+			console.log(result);
+			setFile(null);
+			e.target.reset();
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	// add the url endpoint
+	const handleIdSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch(`${BASE_URL}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ id }),
+			});
+			const result = await response.json();
+			console.log(result);
+			setId('');
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<>
+			<form onSubmit={handleImageSubmit}>
+				<div>
+					<label>Upload an image:</label>
+					<input type="file" accept="image/*" onChange={handleImageChange} />
+				</div>
+				{/* 
+				<div>
+					<label>Select one</label>
+					<select>
+						<option value="imageClassification">Image Classification</option>
+						<option value="objectDetection">Object Detection</option>
+					</select>
+				</div> */}
+
+				<div>
+					<button type="submit">Upload Image</button>
+				</div>
+			</form>
+
+			<form onSubmit={handleIdSubmit}>
+				<div>
+					<label>Enter ID:</label>
+					<input
+						type="text"
+						placeholder="Enter ID"
+						value={id}
+						onChange={(e) => setId(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button type="submit">Submit ID</button>
+				</div>
+			</form>
+		</>
+	);
+};
+
+export default Form;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Form from '../components/Form';
+
+const HomePage = () => {
+	return (
+		<>
+			<Header />
+			<Form />
+		</>
+	);
+};
+export default HomePage;
